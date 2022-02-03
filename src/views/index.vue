ff10c6f757497281e12b011a4a87a51074a01c19
kang9kang9 <kang9kang9@deephigh.net>
2022-02-03 19:01:55 +0900

    주석 달기

--- src/views/index.vue
+++ src/views/index.vue
@@ -70,49 +70,49 @@ export default {
                 dayRecord: today
             })
             this.text=''
-            localStorage.setItem('todos',JSON.stringify(this.todo))
+            localStorage.setItem('todos',JSON.stringify(this.todo)) //list 1개 생성할때 마다 저장
         }
         },
-        toggleCheckbox(index) {
-            this.todo[index].checked = !this.todo[index].checked
-            localStorage.setItem('todos',JSON.stringify(this.todo))
-                if(!this.todo[index].checked){
-                    this.countcheck--
-                    localStorage.setItem('checkcount',JSON.stringify(this.countcheck))
+        toggleCheckbox(index) {  // checkbox 체크박스 클릭 인덱스 
+            this.todo[index].checked = !this.todo[index].checked  //체크 박스 클릭시 반복 클릭시 true에서 false / false 에서 true 로 
+            localStorage.setItem('todos',JSON.stringify(this.todo))  // 체크 박스 상태 변화 저장
+                if(!this.todo[index].checked){ //만약 선택한 인덱스의 체크 박스가 false일때
+                    this.countcheck--  //list의 체크 횟수 차감
+                    localStorage.setItem('checkcount',JSON.stringify(this.countcheck))  //변경된 체크 카운트 저장
                 }else{
-                    this.countcheck++
-                    localStorage.setItem('checkcount',JSON.stringify(this.countcheck))
+                    this.countcheck++ //list의 체크 횟수 추가
+                    localStorage.setItem('checkcount',JSON.stringify(this.countcheck)) //변경된 체크 카운트 저장
                 }
         },
-        deletes(index){
-           this.todo.splice(index, 1)
-           localStorage.setItem('todos',JSON.stringify(this.todo))
-            if(this.todo.length<0){
+        deletes(index){  // 삭제 버튼 클릭 인덱스 
+           this.todo.splice(index, 1)  // 클릭한 인덱스 배열 삭제
+           localStorage.setItem('todos',JSON.stringify(this.todo))  // 변경된 배열 저장
+            if(this.todo.length<0){  //만약 todo가 음수로 가지 않게 예외 처리 및 체크 카운트 감소
                 this.countcheck--
-                localStorage.setItem('checkcount',JSON.stringify(this.countcheck))
-            }else if(this.countcheck!==0){
+                localStorage.setItem('checkcount',JSON.stringify(this.countcheck))  // 변경된 체크 카운트 값 저장
+            }else if(this.countcheck!==0){ //체크카운트가 0이 아닌이상 제거시 같이 감소 
                 this.countcheck--
-                localStorage.setItem('checkcount',JSON.stringify(this.countcheck))
+                localStorage.setItem('checkcount',JSON.stringify(this.countcheck))  // 변경된 체크 카운트 값 저장
             }
         },
-        allDeletes(){
-            this.todo.splice(0)
-            localStorage.setItem('todos',JSON.stringify(this.todo))
-            this.countcheck=0
-            localStorage.setItem('checkcount',JSON.stringify(this.countcheck))
+        allDeletes(){   //전체 삭제 
+            this.todo.splice(0)  // list 전체 삭제
+            this.countcheck=0  //전체 삭제 체크카운터 0으로 초기화
+            localStorage.setItem('todos',JSON.stringify(this.todo))  // 변경된 삭제 리스트 저장
+            localStorage.setItem('checkcount',JSON.stringify(this.countcheck))  // 변경된 체크 카운트 저장
         },
-        selectChange(e){
+        selectChange(e){  //정렬 선택 e 선택한 이벤트 값 불러오기
         console.log(e.target.value)
-        if(e.target.value==='최신순'){
+        if(e.target.value==='최신순'){  
             localStorage.setItem('sort',JSON.stringify(e.target.value))
             this.todo.sort(function(a,b){
-                return a.dayRecord > b.dayRecord ? -1 : a.dayRecord < b.dayRecord ? 1 : 0;
+                return a.dayRecord > b.dayRecord ? -1 : a.dayRecord < b.dayRecord ? 1 : 0;  //최신순으로 정렬
             })
             localStorage.setItem('todos',JSON.stringify(this.todo))
         }else{
             localStorage.setItem('sort',JSON.stringify(e.target.value))
             this.todo.sort(function(a,b){
-                return a.dayRecord < b.dayRecord ? -1 : a.dayRecord > b.dayRecord ? 1 : 0;
+                return a.dayRecord < b.dayRecord ? -1 : a.dayRecord > b.dayRecord ? 1 : 0; //오래된순으로 정렬
         })
             localStorage.setItem('todos',JSON.stringify(this.todo))
         }
